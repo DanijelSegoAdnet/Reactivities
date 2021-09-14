d3eb90bde7b71ad2d30f2de5781f3e8063fd4d77
Danijel Šego <danijel.sego@adnet.hr>
2021-09-14 11:22:55 +0200

    Test 1

--- client-app/src/app/layout/NavBar.tsx
+++ client-app/src/app/layout/NavBar.tsx
@@ -17,7 +17,7 @@ export default observer(function NavBar() {
                 </Menu.Item>
                 <Menu.Item as={NavLink} to='/activities' name='Activities' />
                 <Menu.Item>
-                    <Button as={NavLink} to='/createActivity' positive content='Create Activity' />
+                    <Button as={NavLink} to='/createActivity' positive content='Create Activity B' />
                 </Menu.Item>
                 <Menu.Item position='right'>
                     <Image src={user?.image || '/assets/user.png'} avatar spaced='right' />
